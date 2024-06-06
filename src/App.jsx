10cc47252864fd4de039a7605db180a0891fb201
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2024-06-06 15:05:45 +0530

    bento grid

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Bento from "./components/Bento";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+  <h1>HELLO WORLD!</h1>
+      <Bento/>
+    </div>
+  );
+}
+
+export default App;
